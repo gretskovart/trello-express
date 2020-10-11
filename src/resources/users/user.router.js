7dde19f156e46a06bd66bdda670c45804847d75c
Artem <gretskovart@gmail.com>
2020-10-11 19:48:13 +0400

    feat: add user routes

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -4,8 +4,49 @@ const usersService = require('./user.service');
 
 router.route('/').get(async (req, res) => {
   const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
+
+  res.send(users.map(User.toResponse));
+});
+
+router.route('/:id').get(async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await usersService.get(id);
+
+    res.send(User.toResponse(user));
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+router.route('/').post(async (req, res) => {
+  const { login, password, name } = req.body;
+  const user = await usersService.create(new User({ login, password, name }));
+
+  res.send(User.toResponse(user));
+});
+
+router.route('/:id').put(async (req, res) => {
+  try {
+    const { login, password, name } = req.body;
+    const { id } = req.params;
+    const user = await usersService.update(id, { login, password, name });
+
+    res.send(User.toResponse(user));
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+router.route('/:id').delete(async (req, res) => {
+  try {
+    const { id } = req.params;
+    await usersService.remove(id);
+
+    res.status(204).send();
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
 
 module.exports = router;
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,9 @@
 const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
+const get = id => usersRepo.get(id);
+const create = user => usersRepo.create(user);
+const update = (id, updates) => usersRepo.update(id, updates);
+const remove = id => usersRepo.remove(id);
 
-module.exports = { getAll };
+module.exports = { getAll, get, create, update, remove };
